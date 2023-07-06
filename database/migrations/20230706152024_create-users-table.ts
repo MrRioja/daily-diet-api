04aa14a4222f623485d9bc1171aed0c2062fda00
Luiz Rioja <lulyrioja@gmail.com>
2023-07-06 13:30:10 -0300

    feat(knex): create users table

---
+++ database/migrations/20230706152024_create-users-table.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id').index()
+    table.text('photo_url').nullable()
+    table.text('last_name').notNullable()
+    table.text('first_name').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
