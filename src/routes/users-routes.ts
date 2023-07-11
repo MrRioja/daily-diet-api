4988a8d34cc80444276e17ecb317870f56e8b5be
Luiz Rioja <lulyrioja@gmail.com>
2023-07-11 16:57:48 -0300

    feat(users): create route to get metrics

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -46,4 +46,45 @@ export async function usersRoutes(app: FastifyInstance) {
 
     res.send({ user })
   })
+
+  app.get(
+    '/metrics',
+    {
+      preHandler: [checkSessionIdSession],
+    },
+    async (req: FastifyRequest, res: FastifyReply) => {
+      const { sessionId } = req.cookies
+
+      const { id } = await knex('users')
+        .select('id')
+        .where({ session_id: sessionId })
+        .first()
+
+      const meals = await knex('meals')
+        .select('on_diet')
+        .where({ user_id: id })
+        .orderBy('updated_at')
+
+      let bestSequence = 0
+      let currentSequence = 0
+
+      for (const meal of meals) {
+        if (meal.on_diet === 1) {
+          currentSequence++
+          bestSequence = Math.max(bestSequence, currentSequence)
+        } else {
+          currentSequence = 0
+        }
+      }
+
+      const onDiet = meals.filter((meal) => meal.on_diet).length
+
+      res.send({
+        total: meals.length,
+        onDiet,
+        offDiet: meals.length - onDiet,
+        bestSequence,
+      })
+    },
+  )
 }
